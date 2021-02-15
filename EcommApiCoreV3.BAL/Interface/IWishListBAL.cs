6e42e4a55d006104049896ea8b57bd68ce886333
Sonu Kumar <sonukumar18@gmail.com>
2021-02-15 20:09:17 +0530

    commit WishList code

---
+++ EcommApiCoreV3.BAL/Interface/IWishListBAL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using EcommApiCoreV3.Entities;
+
+namespace EcommApiCoreV3.BAL.Interface
+{
+    public interface IWishListBAL
+    {
+        Task<int> AddToWishList(Cart obj);
+        Task<List<Cart>> DelWishListById(Cart obj);
+        Task<List<Cart>> GetWishListByUserId(Cart obj);
+    }
+}
---
+++ EcommApiCoreV3.BAL/WishListBAL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using EcommApiCoreV3.BAL.Interface;
+using EcommApiCoreV3.Entities;
+using EcommApiCoreV3.Repository.Interface;
+
+namespace EcommApiCoreV3.BAL
+{
+    public class WishListBAL : IWishListBAL
+    {
+        IWishListRepository _IWishListRepository;
+
+        public WishListBAL(IWishListRepository IWishListRepository)
+        {
+            _IWishListRepository = IWishListRepository;
+        }
+
+        public Task<int> AddToWishList(Cart obj)
+        {
+            return _IWishListRepository.AddToWishList(obj);
+        }
+        public Task<List<Cart>> DelWishListById(Cart obj)
+        {
+            return _IWishListRepository.DelWishListById(obj);
+        }
+        public Task<List<Cart>> GetWishListByUserId(Cart obj)
+        {
+            return _IWishListRepository.GetWishListByUserId(obj);
+        }
+    }
+}
