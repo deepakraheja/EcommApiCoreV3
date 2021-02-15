6ad40d619278824865fb54e978c46d5d17b2dea0
Sonu Kumar <sonukumar18@gmail.com>
2021-02-15 20:08:16 +0530

    commit WishList code

---
+++ EcommApiCoreV3.Repository/Interface/IWishListRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using EcommApiCoreV3.Entities;
+
+namespace EcommApiCoreV3.Repository.Interface
+{
+    public interface IWishListRepository
+    {
+        Task<int> AddToWishList(Cart obj);
+        Task<List<Cart>> DelWishListById(Cart obj);
+        Task<List<Cart>> GetWishListByUserId(Cart obj);
+    }
+}
---
+++ EcommApiCoreV3.Repository/WishListRepository.cs
@@ -0,0 +1,61 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EcommApiCoreV3.Entities;
+using EcommApiCoreV3.Repository.Interface;
+using static System.Data.CommandType;
+
+namespace EcommApiCoreV3.Repository
+{
+    public class WishListRepository : BaseRepository, IWishListRepository
+    {
+        public async Task<int> AddToWishList(Cart obj)
+        {
+            try
+            {
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add("@UserId", obj.UserID);
+                parameters.Add("@ProductSizeId", obj.ProductSizeId);
+                var res = await SqlMapper.ExecuteAsync(con, "p_WishList_ins", param: parameters, commandType: StoredProcedure);
+                return res;
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+        }
+
+        public async Task<List<Cart>> DelWishListById(Cart obj)
+        {
+            try
+            {
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add("@WishListId", obj.WishListId);
+                parameters.Add("@UserId", obj.UserID);
+                List<Cart> lst = (await SqlMapper.QueryAsync<Cart>(con, "p_DelWishListById", param: parameters, commandType: StoredProcedure)).ToList();
+                return lst;
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+        }
+
+        public async Task<List<Cart>> GetWishListByUserId(Cart obj)
+        {
+            try
+            {
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add("@UserId", obj.UserID);
+                List<Cart> lst = (await SqlMapper.QueryAsync<Cart>(con, "p_WishList_selbyUserId", param: parameters, commandType: StoredProcedure)).ToList();
+                return lst;
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+        }
+    }
+}
